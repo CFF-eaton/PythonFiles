c75210f94cfdc851087ab952df45c385cd47b3de
CFF-eaton <eaton.sun@gmail.com>
2025-02-20 19:10:54 -0800

    Add files via upload

---
+++ movieslist.py
@@ -0,0 +1,9 @@
+a=input("What is your fav movie #1")
+b=input("What is your fav movie #2")
+c=input("What is your fav movie #3")
+d=input("What is your fav movie #4")
+e=input("What is your fav movie #5")
+favMovies=[a,b,c,d,e]
+
+rev=list(reversed(favMovies))
+print (rev)
